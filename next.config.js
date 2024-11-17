1337779497ac2b72f83ade74aa62a9dda22e50ed
matt20013 <matthew.hodgskiss@gmail.com>
2024-11-17 08:50:04 +0000

    update prefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const withImages = require("next-images");
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+  assetPrefix: isProd ? '/maplewoodband-website' : '',
   reactStrictMode: true,
   output: "export",
   swcMinify: true,
