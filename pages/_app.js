34b01c3fa76c15b51d9330861b3b966c07b2bd51
matt20013 <matthew.hodgskiss@gmail.com>
2024-11-17 10:56:35 +0000

    Static website

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-// pages/_app.js
-//import '../styles/globals.css';
-import '../styles/styles.css';
-import '../node_modules/react-image-gallery/styles/css/image-gallery.css'
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
